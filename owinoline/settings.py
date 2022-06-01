f81d9a227d5b2a3256486652359d269dd9a5b53f
24chris <40150356+24chris@users.noreply.github.com>
2022-06-01 22:39:02 +0300

    img-path

--- owinoline/settings.py
+++ owinoline/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['owinotest.herokuapp.com']
 
 
 # Application definition
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     
     "http://localhost:8080",
+    "https://owinotest.herokuapp.com",
+    "https://owino-24chris.vercel.app",
     # "http://127.0.0.1:9000",
 ]
 
