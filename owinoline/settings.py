b31834b7f6e172c41c0d681fe1f7acd4984ec200
24chris <40150356+24chris@users.noreply.github.com>
2022-06-02 07:05:19 +0300

    cors

--- owinoline/settings.py
+++ owinoline/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['owinotest.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -50,7 +50,7 @@ CORS_ALLOWED_ORIGINS = [
     
     "http://localhost:8080",
     "https://owinotest.herokuapp.com",
-    
+    "https://owino-24chris.vercel.app"
     # "http://127.0.0.1:9000",
 ]
 
